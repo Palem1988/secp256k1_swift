9b5d87f15ddae103252dd587f1aaa79183441cc9
Ben Baron <ben@einsteinx2.com>
2018-02-20 17:29:10 -0500

    Fixed name and target name in Swift 4 to avoid confusion

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "secp256k1",
+    name: "secp256k1_swift",
     pkgConfig: "libsecp256k1",
     providers: [
         .apt(["libsecp256k1"])
@@ -11,11 +11,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "secp256k1",
-            targets: ["secp256k1"]
+            name: "secp256k1_swift",
+            targets: ["secp256k1_swift"]
         )
     ],
     targets: [
-        .target(name: "secp256k1")
+        .target(name: "secp256k1_swift")
     ]
 )
