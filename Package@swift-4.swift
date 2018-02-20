1a280a49f4218c1c6fe51000319c0308ea79f027
Ben Baron <ben@einsteinx2.com>
2018-02-20 17:16:03 -0500

    Attempt to add Swift 4 support

---
+++ Package@swift-4.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "secp256k1",
+    providers: [
+        .apt(["libsecp256k1"])
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "secp256k1",
+            targets: ["secp256k1"]
+        )
+    ],
+    targets: [
+        .target(name: "secp256k1")
+    ]
+)
